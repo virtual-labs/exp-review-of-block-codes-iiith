cbf0afa8f73e04bc5ea0a2797ae5daee72c9f354
vleadadmin <admin@vlabs.ac.in>
2024-03-20 09:37:56 +0000

    https://virtual-labs.github.io/exp-review-of-block-codes-iiith click on the link to test your code.

---
+++ simulation/js/dimension_linear_block.js
@@ -0,0 +1,167 @@
+const generatorMatrices = [
+    {
+        matrix: [
+            [1, 1, 0, 0, 0, 0, 1, 1],
+            [0, 0, 0, 0, 1, 1, 1, 1],
+            [0, 1, 1, 0, 1, 0, 0, 1],
+            [0, 1, 0, 1, 1, 0, 1, 0]
+        ],
+        dim: 4
+    },
+    {
+        matrix: [
+            [0, 1, 1, 1, 0, 1],
+            [1, 1, 1, 0, 1, 0],
+            [1, 1, 0, 0, 0, 1]
+        ],
+        dim: 3
+    },
+    {
+        matrix: [
+            [1, 0, 1, 1, 0, 1],
+            [0, 1, 1, 0, 1, 0],
+            [1, 0, 1, 1, 0, 1]
+        ],
+        dim: 2
+    },
+    {
+        matrix: [
+            [1, 1, 0, 1, 0],
+            [0, 1, 1, 0, 0],
+            [0, 0, 0, 1, 1],
+            [1, 0, 1, 0, 1]
+        ],
+        dim: 3
+    },
+    {
+        matrix: [
+            [1, 1, 0],
+            [0, 0, 1],
+            [0, 1, 0],
+            [1, 0, 1]
+        ],
+        dim: 3
+    }
+];
+
+var randomGeneratorMatrix = selectRandomGeneratorMatrix();
+var dim = randomGeneratorMatrix.dim;
+var codelength = randomGeneratorMatrix.matrix[0].length;
+
+document.getElementById("generatorMatrix").innerHTML = " \\(A \\)= " + formatMatrix(randomGeneratorMatrix.matrix);
+// document.getElementById("matrixInfo").innerHTML = "Dimensions: " + randomGeneratorMatrix.dim.join("x") + ", Length: " + randomGeneratorMatrix.length;
+
+
+function selectRandomGeneratorMatrix() {
+    const randomIndex = Math.floor(Math.random() * generatorMatrices.length);
+    return generatorMatrices[randomIndex];
+}
+
+function formatMatrix(matrix) {
+    return "\\(\\begin{bmatrix} " + matrix.map(row => row.join(" & ")).join("\\\\ ") + " \\end{bmatrix} \\)";
+}
+
+
+function initial() {
+
+    randomGeneratorMatrix = selectRandomGeneratorMatrix();
+    dim = randomGeneratorMatrix.dim;
+    codelength = randomGeneratorMatrix.matrix[0].length;
+
+    const generatorMatrixElement = document.getElementById("generatorMatrix");
+    generatorMatrixElement.innerHTML = " \\(A \\)= " + formatMatrix(randomGeneratorMatrix.matrix);
+
+    // Trigger MathJax to typeset the updated content
+    MathJax.typeset([generatorMatrixElement]);
+
+    const isGeneratorQuestion = document.getElementById("isGeneratorQuestion");
+    const rateQuestion = document.getElementById("rateQuestion");
+    const generatorForm = document.getElementById("generatorForm");
+    const rateForm = document.getElementById("rateForm");
+
+    const dimensionEntered = document.getElementById("dimensionEntered");
+    const rateEntered = document.getElementById("rateEntered");
+
+    dimensionEntered.innerHTML = "Try with new matrix";
+    rateEntered.innerHTML = "";
+
+    isGeneratorQuestion.style.display = "block";
+    rateQuestion.style.display = "none";
+
+    generatorForm.reset();
+    rateForm.reset();
+}
+
+
+function yesValidGeneratorMatrix() {
+    const dim = randomGeneratorMatrix.dim;
+    const dimensionEntered = parseInt(document.getElementById("dimensionInput").value);
+    const isGeneratorQuestion = document.getElementById("isGeneratorQuestion");
+    if (dimensionEntered == dim) {
+        if (dim === randomGeneratorMatrix.matrix.length) {
+            document.getElementById("dimensionEntered").innerHTML = "<b>Correct Answer!!!</b>";
+            document.getElementById("dimensionEntered").style.color = "green";
+
+            isGeneratorQuestion.style.display = "none";
+            rateQuestion.style.display = "block";
+        } else {
+            document.getElementById("dimensionEntered").innerHTML = "<b>Incorrect. Please check if the matrix is full rank.</b>";
+            document.getElementById("dimensionEntered").style.color = "red";
+        }
+    } else {
+        document.getElementById("dimensionEntered").innerHTML = "<b>Incorrect. Please try again.</b>";
+        document.getElementById("dimensionEntered").style.color = "red";
+    }
+
+}
+
+function notValidGeneratorMatrix() {
+    const dim = randomGeneratorMatrix.dim;
+    const dimensionEntered = parseInt(document.getElementById("dimensionInput").value);
+    const isGeneratorQuestion = document.getElementById("isGeneratorQuestion");
+
+    if (dimensionEntered == dim) {
+        if (dimensionEntered != randomGeneratorMatrix.matrix.length) {
+            document.getElementById("dimensionEntered").innerHTML = "<b>Correct Answer!!!</b>";
+            document.getElementById("dimensionEntered").style.color = "green";
+
+            // isGeneratorQuestion.style.display = "none";
+            // rateQuestion.style.display = "block";
+
+            initial();
+        } else {
+            document.getElementById("dimensionEntered").innerHTML = "<b>Incorrect. Please check if the matrix is full rank or not.</b>";
+            document.getElementById("dimensionEntered").style.color = "red";
+        }
+    } else {
+        document.getElementById("dimensionEntered").innerHTML = "<b>Incorrect. Please try again.</b>";
+        document.getElementById("dimensionEntered").style.color = "red";
+    }
+
+}
+
+function checkRate() {
+    const dimensionEntered = document.getElementById("dimensionInput");
+    const rateEntered = document.getElementById("rateEntered");
+
+    const dimensionInput = parseInt(document.getElementById("dimensionInput").value);
+    const codelengthInput = parseFloat(document.getElementById("codelengthInput").value);
+
+    if (dimensionInput == dim && codelengthInput == codelength) {
+        rateEntered.innerHTML = "<b>Correct Answer!!!</b>";
+        rateEntered.style.color = "green";
+    }
+    else {
+        if (rateEntered.innerHTML == "<b>Incorrect. Please try again.</b>") {
+            rateEntered.innerHTML = "<b>Incorrect. Please try again.</b>";
+            rateEntered.style.color = "red";
+            return;
+        }
+        else {
+            rateEntered.innerHTML = "<b>Incorrect. Please try again.</b>";
+            rateEntered.style.color = "red";
+            return;
+        }
+    }
+
+}
---
+++ simulation/js/minimum_distance.js
@@ -0,0 +1,152 @@
+var m1, m2, m3;
+
+var bits = [0, 0, 0]; // Initial bit values
+var codewordBits = [0, 0, 0, 0, 0, 0]; // Initial codeword bit values
+var receivedCodewordBits = [0, 0, 0, 0, 0, 0]; // Initial transmitted codeword bit values
+var receivedCodewordBitsEntered = [0, 0, 0, 0, 0, 0]; // Initial received codeword bit values
+var setOfCodewords = [];
+
+function toggleBit(bitIndex) {
+    // Toggle the bit value
+    bits[bitIndex - 1] = 1 - bits[bitIndex - 1]; // Toggles between 0 and 1
+    document.getElementById('msgBit' + bitIndex).innerText = bits[bitIndex - 1];
+}
+
+function toggleCodewordBit(bitIndex) {
+    // Toggle the bit value
+    codewordBits[bitIndex - 1] = 1 - codewordBits[bitIndex - 1]; // Toggles between 0 and 1
+    document.getElementById('codewordBit' + bitIndex).innerText = codewordBits[bitIndex - 1];
+}
+
+function initial() {
+    const part1 = document.getElementById("part1");
+    const part2 = document.getElementById("part2");
+    const form1 = document.getElementById("form1");
+    const form2 = document.getElementById("form2");
+
+    const helpPrompt = document.getElementById('helpPrompt');
+    const enteredCodewords = document.getElementById('enteredCodewords');
+
+    helpPrompt.innerText = '';
+    enteredCodewords.innerText = '';
+
+    part1.style.display = "block";
+    part2.style.display = "none";
+
+    bits = [0, 0, 0];
+    setOfCodewords = [];
+    codewordBits = [0, 0, 0, 0, 0, 0];
+    receivedCodewordBitsEntered = [0, 0, 0, 0, 0, 0];
+    receivedCodewordBits = [0, 0, 0, 0, 0, 0];
+
+    form1.reset();
+    form2.reset();
+
+}
+
+function submitCodeword() {
+    // console.log("submitCodeword() called");
+    receivedCodewordBitsEntered = codewordBits.slice(0);
+    let hammingWeightEntered = document.getElementById('hammingWeight').value;
+    const helpPrompt = document.getElementById('helpPrompt');
+
+    // check if the codeword is valid
+    let candidates = [];
+    for (let i = 0; i < Math.pow(2, bits.length); i++) {
+        let msg = i.toString(2).padStart(bits.length, "0").split("").map((bit) => parseInt(bit));
+        // let distance = 0;
+        let candidate = [msg[0], msg[1], msg[2], (msg[0] + msg[2]) % 2, (msg[0] + msg[1]) % 2, (msg[1] + msg[2]) % 2];
+        candidates.push(candidate);
+    }
+
+    // console.log(candidates);
+    // console.log(receivedCodewordBitsEntered);
+    // check if the candidate is in the list of candidates
+    let isCorrect = false;
+    for (let i = 0; i < candidates.length; i++) {
+        if (JSON.stringify(candidates[i]) == JSON.stringify(codewordBits)) {
+            isCorrect = true;
+            break;
+        };
+    }
+
+    let alreadyExists = false;
+    // console.log(setOfCodewords.length);
+    // console.log(JSON.stringify(setOfCodewords[i]), JSON.stringify(codewordBits));
+
+    for (let i = 0; i < setOfCodewords.length; i++) {
+        console.log(JSON.stringify(setOfCodewords[i]), JSON.stringify(codewordBits));
+        if (JSON.stringify(setOfCodewords[i]) == JSON.stringify(codewordBits)) {
+            alreadyExists = true;
+            break;
+        };
+    }
+
+    // hamming weight of the codeword is the sum of the bits
+    let hammingWeight = receivedCodewordBitsEntered.reduce((acc, bit) => acc + bit);
+
+    // console.log(candidates, isCorrect);
+
+
+
+    if (isCorrect) {
+
+        if (!alreadyExists && hammingWeight == hammingWeightEntered) {
+            // Clear the error message
+            helpPrompt.innerText = 'You have entered the correct codeword and the Hamming weight.';
+            helpPrompt.style.color = 'green';
+            // Display the list of codewords
+
+            setOfCodewords.push([...codewordBits]);
+
+            document.getElementById('enteredCodewords').innerText = '{' + setOfCodewords.map((codeword) => '(' + codeword.join('') + ')').join(', ') + '}';
+
+        } else if (!alreadyExists && hammingWeight != hammingWeightEntered) {
+            // Display an error message
+            helpPrompt.innerText = 'Oops! The Hamming weight of the codeword is not ' + hammingWeightEntered + '.' + ' Please try again.';
+            helpPrompt.style.color = 'red';
+        
+        }
+        else if (alreadyExists) {
+            // Display an error message
+            helpPrompt.innerText = 'The codeword already exists in the list of codewords.' + ' Enter a different codeword.';
+            helpPrompt.style.color = 'red';
+        }
+
+        // console.log("setOfCodewords", setOfCodewords);
+        // console.log("isCorrect", isCorrect);
+        // console.log("alreadyExists", alreadyExists);
+
+    } else {
+        // Display an error message
+        helpPrompt.innerText = 'Sorry! This codeword does not belong to the set of codewords. Please try again.';
+        helpPrompt.style.color = 'red';
+    }
+    
+    if (setOfCodewords.length == 8) {
+        helpPrompt.innerText = 'You have entered all the codewords.';
+        part1.style.display = "none";
+        part2.style.display = "block";
+    }
+}
+
+
+function checkMinimumDistance() {
+
+    const minimumDistanceEntered = document.getElementById('minimumDistance').value;
+    const minimumDistance = 3;
+    const helpPrompt = document.getElementById('helpPrompt');
+
+    if (minimumDistanceEntered == minimumDistance) {
+        // Clear the error message
+        helpPrompt.innerText = 'You have entered the correct minimum distance.';
+        helpPrompt.style.color = 'green';
+        // Display the list of codewords
+        document.getElementById('enteredCodewords').innerText = '{' + setOfCodewords.map((codeword) => '(' + codeword.join('') + ')').join(', ') + '}';
+    } else {
+        // Display an error message
+        helpPrompt.innerText = 'Oops! The minimum distance is not ' + minimumDistanceEntered + '.' + ' Please try again.';
+        helpPrompt.style.color = 'red';
+    }
+
+}
